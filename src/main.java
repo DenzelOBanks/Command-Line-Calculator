c6284dd056b45c800d21149506f5d4575a809cc3
23366125 <DenzelOBanks@gmail.com>
2025-02-21 21:46:26 +0000

    Addition of Exception Handeling

--- src/main.java
+++ src/main.java
@@ -1,47 +1,49 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter First Number: ");
-        int i = scanner.nextInt();
 
-        System.out.print("Enter Operation: (* / % + - ) ");
-        int k = scanner.next().charAt(0);
+        try {
+            System.out.print("Enter First Number: ");
+            int i = scanner.nextInt();
 
-        System.out.println();
+            System.out.print("Enter Operation: (* / % + - ) ");
+            int k = scanner.next().charAt(0);
 
-        System.out.print("Enter Second Number: ");
-        int j = scanner.nextInt();
+            System.out.print("Enter Second Number: ");
+            int j = scanner.nextInt();
 
+            switch (k) {
 
+                case '+':
+                    System.out.println(i + j);
+                    break;
 
-        switch (k){
+                case '-':
+                    System.out.println(i - j);
+                    break;
 
-            case '+':
-                System.out.println(i + j);
-                break;
+                case '*':
+                    System.out.println(i * j);
+                    break;
 
-            case '-':
-                System.out.println(i - j);
-                break;
+                case '/':
+                    System.out.println(i / j);
+                    break;
 
-            case '*':
-                System.out.println(i * j);
-                break;
+                case '%':
+                    System.out.println(i % j);
+                    break;
 
-            case '/':
-                System.out.println(i / j);
-                break;
+                default:
+                    System.out.println("Not an Integer");
+                    break;
 
-            case '%':
-                System.out.println(i % j);
-                break;
-
-            default:
-                System.out.println("Not an Integer");
-                break;
-
+            }
+        }catch(InputMismatchException e){
+            System.out.println(e);
         }
     }
 }
