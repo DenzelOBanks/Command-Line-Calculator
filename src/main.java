98323bda175755d531792c0e109e54399674b1db
23366125 <DenzelOBanks@gmail.com>
2025-02-21 23:17:20 +0000

    Basic Command line Calculator

--- src/main.java
+++ src/main.java
@@ -4,46 +4,72 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean continueCalculating = true;
 
-        try {
-            System.out.print("Enter First Number: ");
-            int i = scanner.nextInt();
+        while (continueCalculating) {
+            try {
 
-            System.out.print("Enter Operation: (* / % + - ) ");
-            int k = scanner.next().charAt(0);
+                System.out.print("Enter First Number: ");
+                double operand1 = scanner.nextDouble();
 
-            System.out.print("Enter Second Number: ");
-            int j = scanner.nextInt();
+                System.out.print("Enter Operation: (* / % + - ) ");
+                char operator = scanner.next().charAt(0);
 
-            switch (k) {
+                System.out.print("Enter Second Number: ");
+                double operand2 = scanner.nextDouble();
 
-                case '+':
-                    System.out.println(i + j);
-                    break;
+                switch (operator) {
 
-                case '-':
-                    System.out.println(i - j);
-                    break;
+                    case '+':
+                        System.out.println(operand1 + operand2);
+                        break;
 
-                case '*':
-                    System.out.println(i * j);
-                    break;
+                    case '-':
+                        System.out.println(operand1 - operand2);
+                        break;
 
-                case '/':
-                    System.out.println(i / j);
-                    break;
+                    case '*':
+                        System.out.println(operand1 * operand2);
+                        break;
 
-                case '%':
-                    System.out.println(i % j);
-                    break;
+                    case '/':
+                        if (operand2 == 0) {
+                            System.out.println("Error: Cannot divide by zero.");
+                            break;
+                        }
+                        System.out.println(operand1 / operand2);
+                        break;
 
-                default:
-                    System.out.println("Not an Integer");
-                    break;
+                    case '%':
+                        if (operand2 == 0) {
+                            System.out.println("Error: Cannot calculate remainder with zero.");
+                        break;
+                        }
+                        System.out.println(operand1 % operand2);
+                        break;
 
+                    default:
+                        System.out.println("Error: Invalid operator. Please enter one of: +, -, *, /, %.");
+                        break;
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Invalid input. Please enter numbers only.");
+                scanner.next();
             }
-        }catch(InputMismatchException e){
-            System.out.println(e);
+            while(true){
+            System.out.print("Would you like to continue? yes/no ");
+            String input = scanner.next();
+            if(input.equalsIgnoreCase("no")) {
+                continueCalculating = false;
+                break;
+            } else if(input.equalsIgnoreCase("yes")){
+                break;
+                }else{
+                System.out.println("Invalid input. Please enter 'yes' or 'no'.");
+            }
+            }
         }
+        scanner.close();
     }
 }
