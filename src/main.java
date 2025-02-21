61f4cecc615a5cb9e8ccb64e1859a19ae1055b67
23366125 <DenzelOBanks@gmail.com>
2025-02-21 21:27:16 +0000

    Added BaseCode for Calculator

--- src/main.java
+++ src/main.java
@@ -1,2 +1,47 @@
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter First Number: ");
+        int i = scanner.nextInt();
+
+        System.out.print("Enter Operation: (* / % + - ) ");
+        int k = scanner.next().charAt(0);
+
+        System.out.println();
+
+        System.out.print("Enter Second Number: ");
+        int j = scanner.nextInt();
+
+
+
+        switch (k){
+
+            case '+':
+                System.out.println(i + j);
+                break;
+
+            case '-':
+                System.out.println(i - j);
+                break;
+
+            case '*':
+                System.out.println(i * j);
+                break;
+
+            case '/':
+                System.out.println(i / j);
+                break;
+
+            case '%':
+                System.out.println(i % j);
+                break;
+
+            default:
+                System.out.println("Not an Integer");
+                break;
+
+        }
+    }
 }
